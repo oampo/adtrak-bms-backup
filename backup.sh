6826f9664ac2cb4cba2a189be00fd185a7bf9120
Joe Turner <joe@oampo.co.uk>
2017-02-22 11:53:19 +0000

    Fail better.

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-
+set -e
+set -o pipefail
 DATE=`date +%Y-%m-%d`
 FILE=backup-$DATE.json.gz
 firebase database:get / -P firebase-adtrak-bms --token $FIREBASE_TOKEN | gzip > $FILE
-curl -s --user "API:$MAILGUN_KEY" \
+curl --fail -s --user "API:$MAILGUN_KEY" \
     https://api.mailgun.net/v3/sandbox66670e8080dd4553897f19a3513bb637.mailgun.org/messages \
     -F from="Adtrak BMS <adtrak-bms@sandbox66670e8080dd4553897f19a3513bb637.mailgun.org>" \
     -F to=$RECIPIENT \
