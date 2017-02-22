d4fe1b1315439457e22a47dc68c9add7be7521ae
Joe Turner <joe@oampo.co.uk>
2017-02-22 12:02:01 +0000

    More error handling.

--- backup.sh
+++ backup.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 set -e
 set -o pipefail
-DATE=`date +%Y-%m-%d`
+DATE=$(date +%Y-%m-%d)
 FILE=backup-$DATE.json.gz
 firebase database:get / -P firebase-adtrak-bms --token $FIREBASE_TOKEN | gzip > $FILE
-curl --fail -s --user "API:$MAILGUN_KEY" \
+STATUS=$(curl -s -o /dev/stderr -w "%{http_code}" --user "API:$MAILGUN_KEY" \
     https://api.mailgun.net/v3/sandbox66670e8080dd4553897f19a3513bb637.mailgun.org/messages \
     -F from="Adtrak BMS <adtrak-bms@sandbox66670e8080dd4553897f19a3513bb637.mailgun.org>" \
     -F to=$RECIPIENT \
     -F text="Backup for $DATE" \
     -F subject="Backup $DATE" \
-    -F attachment=@$FILE
+    -F attachment=@$FILE)
+
+if test $STATUS -ne 200; then
+    exit 1;
+fi
 
