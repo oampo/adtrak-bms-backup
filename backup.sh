6a73d77f38e58c33fd4174b6620dc5edd173a192
Joe Turner <joe@oampo.co.uk>
2018-10-01 10:38:50 +0100

    Switch to backup V2

--- backup.sh
+++ backup.sh
@@ -2,8 +2,8 @@
 set -e
 set -o pipefail
 DATE=$(date +%Y-%m-%d)
-FILE=backup-$DATE.json.gz
-firebase database:get / -P firebase-adtrak-bms --token $FIREBASE_TOKEN | gzip > $FILE
+FILE=backup-v2-$DATE.json.gz
+firebase database:get / -P adtrak-bms2 --token $FIREBASE_TOKEN | gzip > $FILE
 b2 authorize_account $B2_ID $B2_KEY
 b2 upload_file adtrak-bms-backups $FILE $FILE
 
