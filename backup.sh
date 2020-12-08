ea3076a27a0c14650bc76b28eccdf095c7bc97ff
Joe Turner <joe@oampo.co.uk>
2020-12-08 12:26:36 +0000

    Run firebase through NPM

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@ set -e
 set -o pipefail
 DATE=$(date +%Y-%m-%d)
 FILE=backup-v2-$DATE.json.gz
-firebase database:get / -P adtrak-bms2 --token $FIREBASE_TOKEN | gzip > $FILE
+npm run firebase -- database:get / -P adtrak-bms2 --token $FIREBASE_TOKEN | gzip > $FILE
 b2 authorize_account $B2_ID $B2_KEY
 b2 upload_file adtrak-bms-backups $FILE $FILE
 
